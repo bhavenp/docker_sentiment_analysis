e818e0fb585f81df7951cf4fda4e06006556520d
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-06-04 14:34:57 -0700

    Created file to run Flask application. Home URL works.

---
+++ app/app/app.py
@@ -0,0 +1,35 @@
+# import logging
+
+from flask import Flask, request, jsonify
+
+# from ml_deploy_demo.predict import predict_online
+# from ml_deploy_demo.util.utils import initialize_logging
+
+# logger = logging.getLogger(__name__)
+# initialize_logging(config_path='/app/logging.yaml')
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    msg = "Flask app is up and running!"
+    return jsonify({"msg": msg})
+
+# create route for prediction
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    """Performs an inference
+    """
+    if request.method == "POST":
+        data = request.get_json()
+        # logger.debug(f"Input to predict/: {data}")
+        pred = "Making prediction" #predict_online(data=data["data"])
+        return jsonify({"input": data, "pred": pred})
+
+    if request.method == "GET":
+        msg = "Please compose your request in POST type with data."
+        # logger.debug(f"Wrong request type {request}.")
+        return jsonify({"msg": msg})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
