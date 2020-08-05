14e1aff6be7cb1ce4337ce6acce0e88fcf992334
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-08-05 16:54:48 -0700

    Updated home endpoint tests with test to make sure that we can keep making predictions even after one has been made.

--- tests/test_home_endpoint.py
+++ tests/test_home_endpoint.py
@@ -11,10 +11,19 @@ def test_index(client):
 # test that we can make a prediction using the homepage
 def test_predict_at_index(client):
 	data = {"sentence": "This place is the best!"}
-	response = client.post('/', data=data)
+	response = client.post('/', data=data, follow_redirects=True)
 	assert response.status_code == 200
 
 	# Check that right HTML page is being generated
 	assert b"Your sentence:" in response.data
+
+	# Check that another prediction can be made on this page
+	data = {"sentence": "This place is the worst!"}
+	response = client.post('/', data=data, follow_redirects=True)
+	assert response.status_code == 200
+
+	# Check that right HTML page is being generated
+	assert b"Your sentence:" in response.data
+
 
 
