34d842f0dab8a97e8d28934e6262d051b046bed9
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-08-04 21:17:18 -0700

    App is running again by hard-coding path to model to load.

--- app/ml_model_api/ml_model_blueprint.py
+++ app/ml_model_api/ml_model_blueprint.py
@@ -7,10 +7,8 @@ from app.ml_model_api.predict import predict_online
 
 
 ml_model_bp = Blueprint('ml_model_bp', __name__) # create a Blueprint object
+model_path = '../model_training/models/sentiment_dense_nn.keras'
 
-# helper function so we can make predictions from home/ and predict/ endpoints
-def make_prediction(sentences):
-    return predict_online(data=sentences)
 
 # create 'home' view for testing purposes
 @ml_model_bp.route('/', methods=["GET", "POST"])
@@ -23,7 +21,7 @@ def home():
         sentence = request.form['sentence']
         current_app.logger.info(f"User wants sentiment for '{sentence}'.")
         
-        pred = make_prediction(sentences=[sentence]) # make the prediction for the sentence
+        pred = predict_online(model_path=model_path, data=[sentence]) # make the prediction for the sentence
         pred_score = str(pred[0][0]) # pred is a list with another list inside of it
         current_app.logger.info(f"Sentiment score = {pred_score}")
         
@@ -37,7 +35,7 @@ def predict():
     if request.method == "POST":
         data = request.get_json()
         current_app.logger.debug(f"Input to \"predict\" endpoint: {data['sentences']}")
-        pred = make_prediction(sentences=data["sentences"])
+        pred = predict_online(model_path=model_path, data=data["sentences"])
         current_app.logger.debug(f"Sentiment predictions = {pred}")
         
         return jsonify({"input": data, "pred": pred})
