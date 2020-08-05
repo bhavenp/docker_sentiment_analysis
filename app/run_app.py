46d343aef3d07b870d8593a0e29f40bd7d9480ad
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-08-04 22:21:45 -0700

    'create_app' function can be imported now.

--- app/run_app.py
+++ app/run_app.py
@@ -4,13 +4,24 @@ from flask import Flask
 from utils import initialize_logging
 from ml_model_api import ml_model_bp
 
-if __name__ == "__main__":
+
+def create_app():
 
 	initialize_logging("./logs/logging.yaml") # load and configure logging
 
 	app = Flask(__name__)
 	app.logger.info("Initializing a Flask app...")
 
+	@app.route("/hello")
+	def hello():
+		return "Hello World!"
+
 	app.model_path = '../model_training/models/sentiment_dense_nn.keras'
 	app.register_blueprint(ml_model_bp)
 	app.run(debug=True)
+
+	return app
+
+
+if __name__ == "__main__":
+	create_app()
