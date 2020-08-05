7afc24017b90076dfeb126d9544776fa537f19a0
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-08-05 16:50:27 -0700

    Updated home endpoint tests with test for if prediction works for a sentence.

--- tests/test_home_endpoint.py
+++ tests/test_home_endpoint.py
@@ -6,4 +6,15 @@ def test_hello(client):
 # test that home endpoint is working
 def test_index(client):
 	response = client.get('/')
-	assert response.status_code == 200 
+	assert response.status_code == 200
+
+# test that we can make a prediction using the homepage
+def test_predict_at_index(client):
+	data = {"sentence": "This place is the best!"}
+	response = client.post('/', data=data)
+	assert response.status_code == 200
+
+	# Check that right HTML page is being generated
+	assert b"Your sentence:" in response.data
+
+
