b412d467b680b57b9dacc84fa1b15010cc72b111
bhavenp <3346052+bhavenp@users.noreply.github.com>
2020-08-12 11:49:46 -0700

    UI service can connect to backend ML service to retrieve predictions.

--- ui_app/ui_api/ui_blueprint.py
+++ ui_app/ui_api/ui_blueprint.py
@@ -1,26 +1,38 @@
 import os, sys, logging
 from flask import Blueprint, current_app
-from flask import request, jsonify
+from flask import request as flask_request
+from flask import jsonify
 from flask import render_template
+import requests
 
 
 ui_bp = Blueprint('ui_bp', __name__) # create a Blueprint object
 
 
+def send_prediction_request(sentence):
+    response = requests.post('http://0.0.0.0:8000/predict', 
+        json={"sentences": [sentence]})
+    if response.status_code != 200:
+        return "Error in sending request to model endpoint"
+    
+    json_data = response.json()
+    return str(json_data['pred'][0][0]) # pred is a list with another list inside of it
+
+
 # create 'index' view
 @ui_bp.route('/', methods=["GET", "POST"])
 def home():
-    if request.method == "GET": # user arrives at home page
+    if flask_request.method == "GET": # user arrives at home page
         current_app.logger.info("Request made to home.")
         return render_template('index.html')
 
-    elif request.method == "POST": # user inputted a sentence for prediction
-        sentence = request.form['sentence']
+    elif flask_request.method == "POST": # user inputted a sentence for prediction
+        sentence = flask_request.form['sentence']
         current_app.logger.info(f"User wants sentiment for '{sentence}'.")
         
         # pred = predict_online(model_path=current_app.model_path, data=[sentence]) # make the prediction for the sentence
-        # pred_score = str(pred[0][0]) # pred is a list with another list inside of it
-        pred_score = "1.0"
+        # pred_score = str(pred[0][0]) 
+        pred_score = send_prediction_request(sentence)
         current_app.logger.info(f"Sentiment score = {pred_score}")
         
         return render_template('after_prediction.html', sentence=sentence, pred_score=pred_score)
